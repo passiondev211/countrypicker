2be0cda17ae925d761b029a0cc872f67073635f9
passiondev211 <fullstackdev925@yandex.com>
2019-03-07 11:53:52 +0900

    implemented countypicker

---
+++ src/app/shared/components/phone-input/phone-input.component.spec.ts
@@ -0,0 +1,56 @@
+import { of } from 'rxjs';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DropdownModule } from 'primeng/dropdown';
+import { PhoneInputComponent } from './phone-input.component';
+import { CountriesService } from 'src/app/services';
+
+describe('PhoneInputComponent', () => {
+  let component: PhoneInputComponent;
+  let fixture: ComponentFixture < PhoneInputComponent > ;
+
+  beforeEach(async (() => {
+    TestBed.configureTestingModule({
+        declarations: [PhoneInputComponent],
+        imports: [FormsModule, HttpClientTestingModule, ReactiveFormsModule, DropdownModule],
+        providers: [CountriesService]
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhoneInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.countries = of ([{
+      name: 'United States of America',
+      code: 'US',
+      callingCode: '1',
+      flag: 'null'
+    }]);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('write value shoule set selectedCountry and phone number', () => {
+    component.writeValue('+14244444444');
+    expect(component.selectedCountry.code).toEqual('US');
+  });
+
+  it('validate with incorrect number should work', () => {
+    component.writeValue('+1424354');
+    expect(component.invalid).toEqual(true);
+    expect(component.validate(null)).not.toEqual(null);
+  });
+
+  it('validate with correct number should work', () => {
+    component.writeValue('+14244444444');
+    expect(component.invalid).toEqual(false);
+    expect(component.validate(null)).toEqual(null);
+  });
+
+});
